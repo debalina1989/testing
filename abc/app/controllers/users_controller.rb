ebfa77b33978c5899444f4600ddb91554d4661b7
debalina1989 <lina.mam@gmail.com>
2012-07-15 20:48:05 +0530

    rails work

---
+++ abc/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+def index
+@users=User.all
+end
+
+
+def show
+@user=User.find(params[:user])
+redirect_to users_path
+end
+
+
+def new
+@user=User.new
+end
+
+def create
+User.create(params[:user])
+redirect_to user_path(@user)
+end
+
+def destroy
+@user.destroy
+end
+
+end
