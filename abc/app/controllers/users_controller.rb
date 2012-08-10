b6a5c8a46a99f8d84a365b0178d202d6c651ae29
debalina1989 <lina.mam@gmail.com>
2012-08-10 19:25:00 +0530

    new rails project

--- abc/app/controllers/users_controller.rb
+++ abc/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
 
   def index
     @users=User.all
@@ -41,7 +42,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user=User.find(params[:id])
+    # @user=User.find(params[:id])
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
       sign_in @user
@@ -62,4 +63,22 @@ class UsersController < ApplicationController
     redirect_to signin_path, notice: "Please sign in." unless signed_in?
   end
 
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
+
+  private
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_path, notice: "Please sign in."
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
+  end
 end
